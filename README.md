ec9da3e92050663b9ea00b93f3a8525aa6dc83e6
Mikhail Denisenko <denisenkom@gmail.com>
2013-05-18 00:03:17 -0400

    configured setup.py

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 pytwain
 =======
 
-TWAIN library using ctypes bindings
+TWAIN library implemented using ctypes library
+
+The Python TWAIN module provides an interface to scanners,
+digital cameras and other devices which implement TWAIN,
+for the Windows platform.
+
+It provides the functionality to allow a Python application to connect to the
+scanner/camera and to retrieve images from that device.
--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
+from distutils.core import setup
 
-
-from distutils.core import setup, Extension
-setup(name='twain',
+setup(name='pytwain',
       version='2.0',
-      author='Kevin Gill',
-      author_email='kevin@movieextras.ie',
-      maintainer='Mikhail Denisenko',
-      maintainer_email='denisenkom@gmail.com',
-      url='http://twainmodule.sourceforge.net',
+      author='Mikhail Denisenko',
+      author_email='denisenkom@gmail.com',
+      url='https://github.com/denisenkom/pytwain',
       package_dir={'': 'src'},
       py_modules=['twain'],
       description="TWAIN API for accessing scanners, cameras, etc on Windows",
-      long_description="""
-The Python TWAIN module provides an interface to scanners, digital cameras and other devices which implement TWAIN, for the Windows platform. It provides the functionality to allow a Python application to connect to the scanner/camera and to retrieve images from that device.
-
-The Python TWAIN module supports 32bit Windows only. It does not run on Apple Computer Platforms or on UNIX based Platforms. 
-      """,
       classifiers=[
           'Development Status :: 4 - Beta',
-          'Environment :: Win32 (MS Windows)',
+          'Environment :: MS Windows',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Natural Language :: English',
@@ -27,5 +19,4 @@ The Python TWAIN module supports 32bit Windows only. It does not run on Apple Co
           'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera',
           'Topic :: Multimedia :: Graphics :: Capture :: Scanners',
           ],
-      download_url='https://sourceforge.net/projects/twainmodule/files/',
       )
