663116900f2d3cf45fe0dafa4f273badb8f91efb
Mikhail Denisenko <denisenkom@gmail.com>
2016-01-30 23:40:13 -0500

    improved test

--- tests/test_integ.py
+++ tests/test_integ.py
@@ -1,8 +1,13 @@
 import unittest
+from six.moves import tkinter
 
 
 class MyTestCase(unittest.TestCase):
     def test_dsm(self):
         import twain
-        sm = twain.SourceManager()
-        print(sm.source_list)
+        root = tkinter.Tk()
+        root.title('scan.py')
+        with twain.SourceManager(root) as sm:
+            ds_name = sm.source_list[0]
+            with sm.open_source(str(ds_name)) as ds:
+                pass
