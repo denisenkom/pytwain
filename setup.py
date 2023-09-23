99f6fd5c7023662ca0f75a6e63759a7bac9a4448
Mikhail Denisenko <denisenkom@gmail.com>
2023-09-23 19:51:19 -0400

    Add description to the package
    This description should show up in pypi package index page.
    Followed instructions here: https://packaging.python.org/en/latest/guides/making-a-pypi-friendly-readme/

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 import version
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='pytwain',
       version=version.get_git_version(),
       author='Mikhail Denisenko',
@@ -9,6 +13,8 @@ setup(name='pytwain',
       package_dir={'': 'src'},
       packages=['twain', 'twain.lowlevel'],
       description="TWAIN API for accessing scanners, cameras, etc on Windows",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Win32 (MS Windows)',
